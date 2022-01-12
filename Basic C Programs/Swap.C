e27ba3817b679c8d6e0d666687d882cf6d6e6361
SwaroopCK <swaroopkorde@gmail.com>
2022-01-12 21:03:51 +0530

    Prog 1

---
+++ Basic C Programs/Swap.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a = 20, b = 10;
+    printf("Before Swap a = %d b = %d",a,b);
+
+    a = a + b;// 20 + 10 = 30 
+    b = a - b;// 30 - 20 = 10 
+    a = a - b;// 30 - 10 = 20
+
+    printf("\n Values After Swapping are: a = %d b = %d" , a , b);
+}
